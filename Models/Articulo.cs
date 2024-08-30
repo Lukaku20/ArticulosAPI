6f515b77042cb6574090bd53bfb65de514164c03
Lucas Martin Carballo <lucascarballo8819@gmail.com>
2024-08-30 01:42:46 -0300

    modified null value object

--- Models/Articulo.cs
+++ Models/Articulo.cs
@@ -4,10 +4,10 @@ namespace ArticulosAPI.Models {
      public class Articulo
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Resena { get; set; }
-        public string Contenido { get; set; }
+        public string? Titulo { get; set; }
+        public string? Resena { get; set; }
+        public string? Contenido { get; set; }
         public DateTime Fecha { get; set; }
-        public string UrlImagen { get; set; }
+        public string? UrlImagen { get; set; }
     }
 }
